f9e49a82567ea2efd708e23fb178ed5f1dcaf9ba
KAldhalimi <karrarmajed@gmail.com>
2019-03-04 08:55:50 -0500

    initial commit

---
+++ EntityFrameWorkCap/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameWorkCap
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ EntityFrameWorkCap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntityFrameWorkCap.Startup))]
+namespace EntityFrameWorkCap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
